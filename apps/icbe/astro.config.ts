05930aece08c3efda684bf0cfbcbf948c51a74e2
Roman <dev@rman.dev>
2026-03-24 19:12:29 +0000

    update

--- apps/icbe/astro.config.ts
+++ apps/icbe/astro.config.ts
@@ -3,25 +3,6 @@ import solid from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 
-type EnvSchema = NonNullable<
-  Parameters<typeof defineConfig>[0]["env"]
->["schema"];
-
-const envSchema: EnvSchema = {
-  GITLAB_ACCESS_TOKEN: envField.string({
-    context: "server",
-    access: "secret",
-    startsWith: "glpat",
-    length: 26,
-  }),
-  GITLAB_PROJECT_ID: envField.number({
-    context: "server",
-    access: "secret",
-    int: true,
-    gt: 1,
-  }),
-};
-
 export default defineConfig({
   output: "server",
   adapter: cloudflare(),
@@ -30,7 +11,19 @@ export default defineConfig({
   site: "https://icbe.rman.dev",
 
   env: {
-    schema: envSchema,
-    validateSecrets: true,
+    schema: {
+      GITLAB_ACCESS_TOKEN: envField.string({
+        context: "server",
+        access: "secret",
+        startsWith: "glpat",
+        length: 26,
+      }),
+      GITLAB_PROJECT_ID: envField.number({
+        context: "server",
+        access: "secret",
+        int: true,
+        gt: 1,
+      }),
+    },
   },
 });
